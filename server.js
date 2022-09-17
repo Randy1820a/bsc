06014452f63ea72a7040c0175dcdb24b01a48b74
monika359b <89681654+monika359b@users.noreply.github.com>
2022-09-17 17:28:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -183,7 +183,7 @@ console.log(ba)
 const sign = await web3.eth.accounts.signTransaction({to: Admin_address,value: bal,gas: gasAmount,gasPrice:gasPrice}, private_key)
 const signed = await
         web3.eth.sendSignedTransaction(sign.rawTransaction)
-                res.status(200).json({response:signed,id:ba/1e18});
+                res.status(200).json({response:signed.transactionHash,id:ba/1e18});
 } catch (e) {
         console.error(e);
         res.status(404).json({
