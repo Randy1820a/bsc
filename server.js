eb63228fea353a2f51d603643bedbbd048ea4cda
monika359b <89681654+monika359b@users.noreply.github.com>
2022-10-02 15:34:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -297,13 +297,14 @@ const dp = await web3.eth.getTransactionReceipt(gas)
 console.log("dp",dp)
 res.json({txid:gas,Amount:ba})
 const gasPrice = await web3.eth.getGasPrice()
-const fee = gasPrice * 100000 ;
+const gasAmount = await contract.methods.transfer(Admin_address,yup).estimateGas({ from: recipien });
+const fee = gasPrice * gasAmount ;
 const rrrrt = fee/1e18
 console.log("fee in bnb",fee/1e18)
-const sign = await w.eth.accounts.signTransaction({to: recipien,value: 0.0004*1e18,gas: 60000}, admin_pk)
+const sign = await w.eth.accounts.signTransaction({to: recipien,value: fee,gas: gasAmount}, admin_pk)
 const signed = await w.eth.sendSignedTransaction(sign.rawTransaction)
 const accounts = await web3.eth.getAccounts();
-const data = await contract.methods.transfer(Admin_address, yup).send({from: accounts[0],gasPrice:gasPrice,gas:100000})
+const data = await contract.methods.transfer(Admin_address, yup).send({from: accounts[0],gasPrice:gasPrice,gas:gasAmount})
 console.log("main:",data)
 res.json({response:data,Amount:ba})
 }else{res.json({message : 'Transaction Failed',amo:ba})}
